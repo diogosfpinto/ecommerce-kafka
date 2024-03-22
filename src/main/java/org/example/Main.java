52e1330c674ba6fe1f86a2dd4a7cd6a1a3c2a5a0
diogosfpinto <diogo.sfpinto@gmail.com>
2024-03-22 14:23:59 -0400

    create fraud detector service consumer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,14 +12,16 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
 
-        var value = "2342";
+        var value = "2342,1274";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", 0, value, value);
         producer.send(record, (data, ex) -> {
             if(ex != null){
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset "+ data.offset());
+            System.out.println("sucesso enviando " + data.topic() + ":::partition " +
+                    data.partition() +
+                    " / offset "+ data.offset());
         }).get();
     }
 
